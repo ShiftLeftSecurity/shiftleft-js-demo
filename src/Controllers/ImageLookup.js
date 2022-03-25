7e3ec4692400825d6a29f12700565c642ea37e8c
shiftleft-chuck <100888182+shiftleft-chuck@users.noreply.github.com>
2022-03-25 12:31:57 -0400

    Update ImageLookup.js

--- src/Controllers/ImageLookup.js
+++ src/Controllers/ImageLookup.js
@@ -7,9 +7,16 @@ class ImageLookup {
     /* Can read any file in the server by passing the filename (image) in the query params */
     /* ex: http GET http://localhost:8089/api/v1/image-lookup image=="package.json" */
     const fileContent = fs.readFileSync(req.query.image).toString();
-    logger.debug(fileContent);
+    string sanitizedString = sanitizeString(fileContent)
+    logger.debug(sanitizedString);
+//    logger.debug(fileContent);
     res.send(fileContent);
   }
+  
+  function sanitizeString(str){
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+    return str.trim();
+}
 }
 
 module.exports = ImageLookup;
