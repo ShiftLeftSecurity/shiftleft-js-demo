85c2e1dc2a8c8d5cb899b719a71dfdaa55504578
Olof-Joachim Frahm (欧雅福) <olof-joachim.frahm@harness.io>
2026-04-15 14:27:58 +0200

    OSS risks demo

---
+++ app.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	_ "github.com/tidwall/gjson"
+	"github.com/julienschmidt/httprouter"
+
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/setting"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/setup"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/user"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/util"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/util/config"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/util/middleware"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/vulnerability/csa"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/vulnerability/idor"
+	pathTraversal "github.com/ShiftLeftSecurity/shiftleft-go-demo/vulnerability/path-traversal"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/vulnerability/sqli"
+	"github.com/ShiftLeftSecurity/shiftleft-go-demo/vulnerability/xss"
+)
+
+const (
+	banner = `
+     ÛÛÛÛÛÛÛÛÛ           ÛÛÛÛÛ   ÛÛÛÛÛ ÛÛÛÛÛ   ÛÛÛ   ÛÛÛÛÛ   ÛÛÛÛÛÛÛÛÛ
+    ÛÛÛ°°°°°ÛÛÛ         °°ÛÛÛ   °°ÛÛÛ °°ÛÛÛ   °ÛÛÛ  °°ÛÛÛ   ÛÛÛ°°°°°ÛÛÛ
+   ÛÛÛ     °°°   ÛÛÛÛÛÛ  °ÛÛÛ    °ÛÛÛ  °ÛÛÛ   °ÛÛÛ   °ÛÛÛ  °ÛÛÛ    °ÛÛÛ
+  °ÛÛÛ          ÛÛÛ°°ÛÛÛ °ÛÛÛ    °ÛÛÛ  °ÛÛÛ   °ÛÛÛ   °ÛÛÛ  °ÛÛÛÛÛÛÛÛÛÛÛ
+  °ÛÛÛ    ÛÛÛÛÛ°ÛÛÛ °ÛÛÛ °°ÛÛÛ   ÛÛÛ   °°ÛÛÛ  ÛÛÛÛÛ  ÛÛÛ   °ÛÛÛ°°°°°ÛÛÛ
+  °°ÛÛÛ  °°ÛÛÛ °ÛÛÛ °ÛÛÛ  °°°ÛÛÛÛÛ°     °°°ÛÛÛÛÛ°ÛÛÛÛÛ°    °ÛÛÛ    °ÛÛÛ
+   °°ÛÛÛÛÛÛÛÛÛ °°ÛÛÛÛÛÛ     °°ÛÛÛ         °°ÛÛÛ °°ÛÛÛ      ÛÛÛÛÛ   ÛÛÛÛÛ
+     °°°°°°°°°   °°°°°°       °°°           °°°   °°°      °°°°°   °°°°° `
+)
+
+//index and set cookie
+
+func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
+	util.SetCookieLevel(w, r, "low") //set cookie Level default to low
+
+	data := make(map[string]interface{})
+	data["title"] = "Index"
+
+	util.SafeRender(w, r, "template.index", data)
+}
+
+func main() {
+
+	fmt.Println(banner)
+
+	mw := middleware.New()
+	router := httprouter.New()
+	user := user.New()
+	sqlI := sqli.New()
+	xss := xss.New()
+	idor := idor.New()
+	csa := csa.New()
+	pathTraversal := pathTraversal.New()
+	setup := setup.New()
+	setting := setting.New()
+
+	router.ServeFiles("/public/*filepath", http.Dir("public/"))
+	router.GET("/", mw.LoggingMiddleware(mw.AuthCheck(indexHandler)))
+	router.GET("/index", mw.LoggingMiddleware(mw.DetectSQLMap(mw.AuthCheck(indexHandler))))
+
+	user.SetRouter(router)
+	sqlI.SetRouter(router)
+	xss.SetRouter(router)
+	idor.SetRouter(router)
+	csa.SetRouter(router)
+	pathTraversal.SetRouter(router)
+	setup.SetRouter(router)
+	setting.SetRouter(router)
+
+	s := http.Server{
+		Addr:    ":8082",
+		Handler: router,
+	}
+
+	fmt.Printf("Server running at port %s\n", s.Addr)
+	fmt.Printf("Open this url %s on your browser to access GoVWA", config.Fullurl)
+	fmt.Println("")
+	s.ListenAndServe()
+
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/ShiftLeftSecurity/shiftleft-go-demo
+
+go 1.12
+
+require (
+	// hijackable
+	github.com/armadanet/appmanager v0.0.0-20211018042743-e8b3a2ce242a
+	// abandoned
+	github.com/passone/helm v1.2.1
+	// squatted package
+	github.com/spf133/cobra v0.0.1
+)
